f8bb9650fd5fcc0635436fe35767da87f5ed2463
王博 <wangbo3919@163.com>
2019-10-21 15:39:56 +0800

    no message

---
+++ WBNoData.h
@@ -0,0 +1,15 @@
+//
+//  WBNoData.h
+//  Pods
+//
+//  Created by 王博 on 2019/10/21.
+//
+
+#ifndef WBNoData_h
+#define WBNoData_h
+
+#import "UICollectionView+NoData.h"
+#import "UITableView+NoData.h"
+#import "IWBNoDataViewProtocol.h"
+
+#endif /* WBNoData_h */
