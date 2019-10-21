28835818569b5038237e2a236226d43005608d04
王博 <wangbo3919@163.com>
2019-10-21 10:44:11 +0800

    no message

--- Example/WBNoDataView/WBViewController.m
+++ Example/WBNoDataView/WBViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "WBViewController.h"
+#import <WBNoDataView.h>
 
 @interface WBViewController ()
 
@@ -18,6 +19,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
 }
 
 - (void)didReceiveMemoryWarning
--- WBNoDataView/Classes/WBNoDataView/WBNoDataView.h
+++ WBNoDataView/Classes/WBNoDataView/WBNoDataView.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "MJRefresh.h"
+#import <MJRefresh/MJRefresh.h>
 NS_ASSUME_NONNULL_BEGIN
 
 extern NSString * const kWBNoDataViewObserveKeyPath;
